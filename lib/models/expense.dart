f1ff11d74000744164156116cdc7132186abb7b2
masanao10337 <ma-takahashi@croooober.co.jp>
2023-12-01 13:10:04 +0900

    make exepnses model

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,16 +1,20 @@
 import 'package:uuid/uuid.dart';
 
-const  uuid = Uuid();
+const uuid = Uuid();
+enum Category { food, travel, leisure, work }
 
 class Expense {
   Expense({
     required this.title,
     required this.ammount,
     required this.date,
+    required this.category,
   }) : id = uuid.v4();
+
   //NOTE: idもmodelingできるかもね。
   final String id;
   final String title;
   final double ammount;
   final DateTime date;
+  final Category category;
 }
