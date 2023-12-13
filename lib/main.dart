9153c047421b6941f74e125a70cb0f0f629c8d19
masanao10337 <ma-takahashi@croooober.co.jp>
2023-12-13 11:42:16 +0900

    Theming app

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,43 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(252, 121, 224, 200),
 );
 
+var kDarkColorSchme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(250, 1, 27, 21),
+);
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorSchme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kDarkColorSchme.primaryContainer,
+          foregroundColor: kDarkColorSchme.onPrimaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorSchme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 6,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorSchme.primaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
-          foregroundColor: kColorScheme.onTertiary,
           backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
         ),
         cardTheme: const CardTheme().copyWith(
           color: kColorScheme.secondaryContainer,
@@ -36,12 +61,13 @@ class MainApp extends StatelessWidget {
         ),
         textTheme: ThemeData().textTheme.copyWith(
               titleLarge: TextStyle(
-                fontWeight: FontWeight.normal,
-                color: kColorScheme.onSecondary,
-                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 16,
               ),
             ),
       ),
+      themeMode: ThemeMode.system,
       home: const Expenses(),
     );
   }
